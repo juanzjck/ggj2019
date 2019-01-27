619bf8a5cdd488f22da5f7545c8657aa60aab73e
Juan Salazar <juanzjck1996@gmail.com>
2019-01-27 08:53:29 -0500

    Todo

--- Script/Player2D.cs
+++ Script/Player2D.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using DG.Tweening;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
@@ -9,34 +10,89 @@ public class Player2D : MonoBehaviour {
     public   float I;
     public float v;
     public int tipoPared;
+    public GameObject[] vidas;
+    int vida;
+    public GameObject candale;
+    public bool encendido;
+    public float scalaMaxima;
+    public float scalaMinima;
+    public GameObject gameover;
+    public int cerrillos;
+    public bool llave;
     // Use this for initialization
     void Start () {
-        I = 0;
+        gameover.SetActive(false);
+       I = 0;
         v = 0;
-	}
+        vidas = GameObject.FindGameObjectsWithTag("Vida");
+        vida = vidas.Length;
+    }
     public void flop() {
         if (CrossPlatformInputManager.GetAxis("Horizontal") > 0)
         {
-            player.GetComponent<Transform>().localScale = new Vector3(1,1,1);
+            player.GetComponent<SpriteRenderer>().flipX = false;
         }
         else {
-            player.GetComponent<Transform>().localScale = new Vector3(-1, 1, 1);
+            if (CrossPlatformInputManager.GetAxis("Horizontal") < 0) {
+                player.GetComponent<SpriteRenderer>().flipX = true;
+            }
+               
+        }
+    }
+    public void gameOver() {
+        if (vida==1) {
+            gameover.SetActive(true);
+            player.SetActive(false);
+            Destroy(vidas[0]);
         }
     }
 	// Update is called once per frame
 	void Update () {
+
+       
+        if (Input.GetKeyDown(KeyCode.Space) && encendido==false) {
+            if (cerrillos>0) {
+                encender();
+            }
+           
+         
+            encendido = true;
+        }
+        if (candale.transform.localScale==new Vector3(scalaMaxima, scalaMaxima, scalaMaxima)) {
+            candale.transform.DOScaleX(scalaMinima, 10);
+            candale.transform.DOScaleY(scalaMinima, 10);
+            candale.transform.DOScaleZ(scalaMinima, 10);
+            
+
+        }
+        if (candale.transform.localScale == new Vector3(scalaMinima, scalaMinima, scalaMinima)) {
+            encendido = false;
+        }
         flop();
         control();
+        gameOver();
         I = CrossPlatformInputManager.GetAxis("Vertical");
         v = CrossPlatformInputManager.GetAxis("Horizontal");
     }
    
     public void OnTriggerEnter2D(Collider2D other)
     {
+
          if (other.gameObject.tag=="Pared") {
            Debug.Log("OnCollisionEnter2D");
             tipoPared= other.gameObject.GetComponent<Pared>().tipo;
         }
+        if (other.gameObject.tag == "Obstaculo") {
+            Debug.Log("restar");
+            vidaRestar();
+        }
+    }
+    public void vidaRestar() {
+        vidas = GameObject.FindGameObjectsWithTag("Vida");
+        vida = vidas.Length;
+        Destroy(vidas[vida-1]);
+        vidas = GameObject.FindGameObjectsWithTag("Vida");
+        vida = vidas.Length;
     }
     public void OnTriggerExit2D(Collider2D other)
     {
@@ -44,6 +100,12 @@ public class Player2D : MonoBehaviour {
             Debug.Log("out");
             tipoPared = 0;
 
+    }
+    public void encender() {
+        candale.transform.DOScaleX(scalaMaxima, 1);
+        candale.transform.DOScaleY(scalaMaxima, 1);
+        candale.transform.DOScaleZ(scalaMaxima, 1);
+        cerrillos --;
     }
     public void control() {
 
